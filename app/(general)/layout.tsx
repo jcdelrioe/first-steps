df413bded0c1a67e2698921da76df0399fb0e783
Juan Carlos Del Rio <jcdre@hotmail.com>
2024-01-05 12:16:46 -0500

    Navbar

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,12 +1,16 @@
-export default function AboutLayout({
+import { Navbar } from "@/components/navbar/Navbar"
+export default function GeneralLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-lg">Hola mundillo</span>
-      {children}
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span className="text-lg">Hola mundillo</span>
+        {children}
+      </main>
+    </>
   )
 }
